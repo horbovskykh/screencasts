43195b767d7bd819cb0508c260d7c7612906bf68
Viacheslav <slav.horbovskykh@gmail.com>
2015-12-21 23:40:04 +0200

    Episode 33. Reflection (method_missing) ep.32 skipped - constants

--- storeapp/init.rb
+++ storeapp/init.rb
@@ -10,3 +10,13 @@ require_relative "virtual_item"
 @items << VirtualItem.new({ :price => 10, :name => "car" })
 @items << RealItem.new({ :weight => 100, :name => "Car", price: 100})
 @items << RealItem.new({ :price => 10, :weight => 100, :name => "Phone" })
+
+cart = Cart.new("slava")
+cart.add_item VirtualItem.new({ :price => 10, :name => "car" })
+cart.add_item RealItem.new({ :weight => 100, :name => "Car", price: 100})
+cart.add_item RealItem.new({ :weight => 100, :name => "car", price: 100})
+cart.add_item RealItem.new({ :weight => 100, :name => "caar", price: 100})
+
+
+p cart.all_caars
+p cart.all_Cars
