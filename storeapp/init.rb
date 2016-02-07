b323ba290de918a0e1d3254bbd067e8c7298155e
Viacheslav <slav.horbovskykh@gmail.com>
2016-02-07 21:39:28 +0200

    Episodes 37-40. DSL - domain specific language and freeze of class (e38 - instance variables in class, e39 - threads)

--- storeapp/init.rb
+++ storeapp/init.rb
@@ -1,8 +1,24 @@
 require_relative "store_application"
+StoreApplication.config do |app|
 
-StoreApplication.new
-StoreApplication.new
-StoreApplication.new
+  app.name = "My store"
+  app.environment = :test
+  
+  app.admin do |admin|
+    admin.email = "admin@mystore.com"
+	admin.login = "admin"
+	admin.send_info_emails_on :mondays
+  end
+end
+
+unless StoreApplication.frozen?
+  StoreApplication.name = "my name"
+end
+
+p StoreApplication.environment
+p StoreApplication.name
+p StoreApplication::Admin.email
+p StoreApplication::Admin.login
 
 @items = []
 @items << VirtualItem.new(price: 10, name: "car") #syntax sugar
@@ -13,6 +29,3 @@ cart = Cart.new("slava")
 cart.add_item VirtualItem.new({ :price => 10, :name => "car" })
 cart.add_item RealItem.new({ :weight => 100, :name => "Car", price: 100})
 cart.add_item RealItem.new({ :weight => 100, :name => "car", price: 100})
-
-method = "all_cars"
-puts cart.send(method)
--- storeapp/store_application.rb
+++ storeapp/store_application.rb
@@ -2,8 +2,9 @@ class StoreApplication
 
   class << self
   
-    def new
+    def config
 	  unless @instance
+	    yield (self)
 	    puts "loading files..."
 	    require_relative "string"
   	    require_relative "item_container"
@@ -14,8 +15,34 @@ class StoreApplication
 	    require_relative "virtual_item"
 	  end
 	  @instance ||= self
+	  @instance.freeze #freeze in method New
+	end
+	
+	attr_accessor :name, :environment
+	
+	def admin(&block)
+	  @admin ||= Admin.new(&block)
 	end
 	
   end
 
+  	class Admin
+	
+	  class << self
+	  def new
+	    unless @instance
+		  yield(self)
+		end
+		@instance ||=self
+		@instance.freeze #freeze in method New
+	  end
+	  
+	  attr_accessor :email, :login
+	  
+	  def send_info_emails_on (day)
+	    @send_info_emails_on = day
+	  end
+	  
+	  end
+	end
 end
